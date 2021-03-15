300129277e007fd614bab7b8b06bde642e3993b4
lamiasristy <lamiasristy@gmail.com>
2021-03-15 17:35:56 +0100

    Add description and live links to the README file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,8 +10,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'https://trackreading.herokuapp.com', 'http://localhost:3001'
 
     resource '*',
-            headers: :any,
-            methods: [:get, :post, :put, :patch, :delete, :options, :head],
-            credentials: true
+          headers: :any,
+          methods: %i[get post put patch delete options head],
+          credentials: true
   end
 end
