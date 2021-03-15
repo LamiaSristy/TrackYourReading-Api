18419974a3a407e1658095e5811b66baed4e90bb
lamiasristy <lamiasristy@gmail.com>
2021-03-15 14:29:38 +0100

    Change in CORS origin points.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://trackreading.herokuapp.com/'
+    origins 'https://trackreading.herokuapp.com', 'http://localhost:3001'
 
     resource '*',
             headers: :any,
